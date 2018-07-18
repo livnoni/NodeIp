fa3727d8507ce5fec0cec99fa274e30851235572
livnoni <yehuda.livnoni@gmail.com>
2018-07-18 21:11:35 +0300

    add all the requests

--- server.js
+++ server.js
@@ -2,18 +2,31 @@ var express = require('express');
 var app = express();
 
 
-var counter = 0;
+var prevCounter = 0;
+var clientsData = [];
 
-app.get('/total', async function (req, res) {
-    console.log("got /total request.");
-    // console.log("req.connection=\n"+JSON.stringify(req.connection));
+app.get('/prev', async function (req, res) {
+    console.log("got /prev GET request.");
+    prevCounter++;
     var ip = req.header('x-forwarded-for') || req.connection.remoteAddress;
-
-
-    console.log("ip=",ip)
-    // console.log("req.connection.remoteAddress=\n"+JSON.stringify(req.connection.remoteAddress));
+    clientsData.push({ip:ip, time: new Date()});
 
     res.end(`this is the ${++counter} time you log in...`);
+    if(clientsData.length == 0){
+        res.end(`You (${clientsData[0].ip}) are the first client that connected, no prev client`);
+    }else{
+        res.end(`the prev ip that connected the server is ${clientsData[clientsData.length - 1].ip}`);
+    }
+});
+
+app.get('/total', async function (req, res) {
+    console.log("got /total GET request.");
+    res.end(`There were total ${prevCounter} of /prev requests served so far.`);
+});
+
+app.get('/stats ', async function (req, res) {
+    console.log("got /stats GET request.");
+    res.end(`stats = \n${JSON.stringify(clientsData)}`);
 });
 
 
