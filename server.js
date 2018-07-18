2e62cd9241f4ddabb89b91c03589635aad4df392
livnoni <yehuda.livnoni@gmail.com>
2018-07-18 22:23:23 +0300

    added data to fetch from req

--- server.js
+++ server.js
@@ -1,37 +1,61 @@
+//https://nodeip.herokuapp.com/
 var express = require('express');
 var app = express();
+var device = require('express-device');
+app.use(device.capture());
 
 
 var prevCounter = 0;
-var clientsData = [];
+var lastClientIp;
+var clientsData = {};
 
-app.get('/prev', async function (req, res) {
+app.get('/prev', function (req, res) {
     console.log("got /prev GET request.");
     prevCounter ++;
     var ip = req.header('x-forwarded-for') || req.connection.remoteAddress;
-    clientsData.push({ip:ip, time: new Date()});
 
-    if(clientsData.length == 0){
-        res.end(`You (${clientsData[0].ip}) are the first client that connected, no prev client`);
-    }else{
-        res.end(`the prev ip that connected the server is ${clientsData[clientsData.length - 1].ip}`);
+    console.log("Hi to "+req.device.type.toUpperCase()+" User");
+    console.log("req.device=\n"+JSON.stringify(req.device));
+    console.log("req.cookies=\n"+JSON.stringify(req.cookies));
+    console.log("req.hostname=\n"+JSON.stringify(req.hostname));
+    console.log("req.ip=\n"+JSON.stringify(req.ip));
+    console.log("req.method=\n"+JSON.stringify(req.method));
+    console.log("req.originalUrl=\n"+JSON.stringify(req.originalUrl));
+    console.log("req.params=\n"+JSON.stringify(req.params));
+    console.log("req.protocol=\n"+JSON.stringify(req.protocol));
+    console.log("req.secure=\n"+JSON.stringify(req.secure));
+    console.log("req.sessionID =\n"+JSON.stringify(req.sessionID ));
+
+
+    if(! clientsData[ip]){
+        clientsData[ip] = {};
+        clientsData[ip].times = [];
     }
+    clientsData[ip].times.push(new Date());
+
+    if(lastClientIp){
+        res.end(`The prev ip that connected the server is ${lastClientIp}`);
+    }else{
+        res.end(`You (${ip}) are the first client that connected, no prev client`);
+    }
+
+    lastClientIp = ip;
+
 });
 
-app.get('/total', async function (req, res) {
+app.get('/total', function (req, res) {
     console.log("got /total GET request.");
     res.end(`There were total ${prevCounter} of /prev requests served so far.`);
 });
 
-app.get('/stats', async function (req, res) {
+app.get('/stats', function (req, res) {
     console.log("got /stats GET request.");
 
-    if(clientsData.length == 0){
-        res.end(`We can not display statistics because there are no clients data.`);
-    }else{
+    if(Object.keys(clientsData).length > 0){
         res.end(`stats = \n${JSON.stringify(clientsData)}`);
+    }else{
+        res.end(`We can not display statistics because there are no clients data.`);
     }
-
 });
 
 
