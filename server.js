0598f9d26dcf103e56664c0edce9512424b7fe0a
livnoni <yehuda.livnoni@gmail.com>
2018-07-18 22:43:32 +0300

    finish project.

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
-//https://nodeip.herokuapp.com/
 var express = require('express');
 var app = express();
-var device = require('express-device');
+var device = require('express-device'); //nice module to get device type
 app.use(device.capture());
 
 
@@ -12,29 +11,16 @@ var clientsData = {};
 app.get('/prev', function (req, res) {
     console.log("got /prev GET request.");
     prevCounter ++;
+
     var ip = req.header('x-forwarded-for') || req.connection.remoteAddress;
 
-    console.log("Hi to "+req.device.type.toUpperCase()+" User");
-    console.log("req.device=\n"+JSON.stringify(req.device));
-    console.log("req.cookies=\n"+JSON.stringify(req.cookies));
-    console.log("req.hostname=\n"+JSON.stringify(req.hostname));
-    console.log("req.ip=\n"+JSON.stringify(req.ip));
-    console.log("req.method=\n"+JSON.stringify(req.method));
-    console.log("req.originalUrl=\n"+JSON.stringify(req.originalUrl));
-    console.log("req.params=\n"+JSON.stringify(req.params));
-    console.log("req.protocol=\n"+JSON.stringify(req.protocol));
-    console.log("req.secure=\n"+JSON.stringify(req.secure));
-    console.log("req.sessionID =\n"+JSON.stringify(req.sessionID ));
-
-
     if(! clientsData[ip]){
-        clientsData[ip] = {};
-        clientsData[ip].times = [];
+        clientsData[ip] = [];
     }
-    clientsData[ip].times.push(new Date());
+    clientsData[ip].push({time: new Date(), deviceType: req.device.type, protocol:req.protocol, isSecureConnection:req.secure});
 
     if(lastClientIp){
-        res.end(`The prev ip that connected the server is ${lastClientIp}`);
+        res.end(`The prev ip that have been connected to the server is ${lastClientIp}`);
     }else{
         res.end(`You (${ip}) are the first client that connected, no prev client`);
     }
@@ -52,13 +38,11 @@ app.get('/stats', function (req, res) {
     console.log("got /stats GET request.");
 
     if(Object.keys(clientsData).length > 0){
-        res.end(`stats = \n${JSON.stringify(clientsData)}`);
+        res.end(`stats = \n${JSON.stringify(clientsData, null, 4)}`);
     }else{
         res.end(`We can not display statistics because there are no clients data.`);
     }
 });
-
-
 
 // Initialize the app.
 var server = app.listen(process.env.PORT || 8080, function () {
